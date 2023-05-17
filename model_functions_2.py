00948263fb01831098e667b516dbd710d79dbee7
Natalia Glazman <glazmann@yahoo.co.uk>
2023-05-17 13:08:10 +0100

    general clean-up and adding error bars to plots

--- model_functions_2.py
+++ model_functions_2.py
@@ -13,16 +13,12 @@ import matplotlib.pyplot as plt
 from matplotlib.pyplot import figure
 from IPython.display import set_matplotlib_formats
 from sklearn.utils import resample as bootstrap
-from scipy.stats.distributions import  t
-import lmfit
-from lmfit import Parameters
 set_matplotlib_formats('svg')
 
 
 
-
 #Import data
-data_file_name = '/Users/nataliaglazman/Library/Mobile Documents/com~apple~CloudDocs/Desktop/FYP/Database.csv'
+data_file_name = 'Database_v2.csv'
 
 data = pd.read_csv(data_file_name)
 
@@ -44,15 +40,18 @@ organism_list = data.interactor1.unique()
 
 h = 6.62607015e-34
 kB = 1.380649e-23
+k = 8.62e-05
 R = 8.314
 
 
 #Define and fit model
 
-Topt_dictionary = {}
-Topt_estim_dictionary = {}
+
 organism_enzyme_dict = {}
-Tinf_dictionary = {}
+Tinf_list = []
+Topt_error_list =[]
+Topt_list = []
+Tinf_error_list = []
 parameter_list = []
 deltaH_list = []
 deltaC_list = []
@@ -63,8 +62,8 @@ deltaS_error = []
         
 
 def fit_model(data, plot_data, bootstrapping):
-    x0 = 10, -100,1.3,-100
-    #x1 = 1,1,1,1
+    x0 = -100,1.3,-100
+    x1 = 1400, 0.58, 2.2, 0.085
     figure(figsize=(12, 10), dpi=3000)
     
     
@@ -72,7 +71,7 @@ def fit_model(data, plot_data, bootstrapping):
         
         
         data_set = data.loc[data['originalid'] == i]
-        T0 = data_set['tempkelvin'].loc[data_set['originaltraitvalue'].idxmax()] - 4
+        T0 = data_set['tempkelvin'].loc[data_set['originaltraitvalue'].idxmax()] + 4
         Tmax = data_set['tempkelvin'].loc[data_set['originaltraitvalue'].idxmax()]
         tmin = data_set['tempkelvin'].min() 
         tmax = data_set['tempkelvin'].max()
@@ -89,43 +88,56 @@ def fit_model(data, plot_data, bootstrapping):
         t_true = data_set['tempkelvin'].array
         
         
+        tm = 340
         
         #Defining the model
         
-        def model_Hobbs(T,p, deltaH, deltaC, deltaS):
-            A = ((kB*T)/(p*h)) * np.exp(-((deltaH+(deltaC*(T-T0)))/(R*T)) + ((deltaS+(deltaC*np.log(T/T0)))/R))
+        def model_Hobbs(T, deltaH, deltaC, deltaS):
+            A = ((kB*T)/(h)) * np.exp(-((deltaH+(deltaC*(T-T0)))/(R*T)) + ((deltaS+(deltaC*np.log(T/T0)))/R))
             return A
         
-        def model_EEAR(T, A0, Eb, EDH, EDC):
-            A = A0*np.exp(-Eb-(EDH*(1-T/T0)+(EDC*(T-T0-(T*np.log(T/T0))))))/kB*T
+        def model_EAAR(T, A0, eb, ef, ehc):  
+            A = A0*np.exp(-((eb-(ef*(1-(T/tm)))+(ehc*(T-tm-(T*np.log(T/tm)))))/(k*T)))
             return A
-        
         
         
         #Error propagation and error calculation functions
     
-        def propagate_errors(T, diag_cov):
+        def propagate_errors(T, diag_cov, p):
             dH = -1/(R*T)
             dC = (T-T0)/(R*T) + np.log(T/T0)/R
             dS = 1/R
+            #dp = -1/p
             derivatives = np.array([dH, dC, dS])
             variance = np.dot(diag_cov, derivatives**2) 
             return np.sqrt(variance)
         
         
-        def calculate_sigma():
-            diff = []
-            for i in range(len(t_true)-1):
-                           
-               diff.append(t_true[i+1] - t_true[i])
-               
-            mean = sum(diff)/len(diff)
-            sigma = (mean/Tmax) * data_set['originaltraitvalue']
+        def propagate_errors_2(T, diag_cov, A0):
+            dA0 = 1/A0
+            dEb = -1/(kB*T)
+            dEDH = (1-(T/T0))/(kB*T)
+            dEDC = (T-T0-(T*np.log(T/T0)))/(kB*T)
+            derivatives = np.array([dEb, dEDH, dEDC, dA0])
+            variance = np.dot(diag_cov, derivatives**2)
+            return np.sqrt(variance)
             
-            return sigma
         
         
-        sigma = calculate_sigma()
+        
+        # def calculate_sigma():
+        #     diff = []
+        #     for i in range(len(t_true)-1):
+                           
+        #         diff.append(t_true[i+1] - t_true[i])
+               
+        #     mean = sum(diff)/len(diff)
+        #     sigma = (mean/Tmax) * data_set['originaltraitvalue']
+            
+        #     return sigma
+        
+        
+        # sigma = calculate_sigma()
         
         
         
@@ -135,56 +147,41 @@ def fit_model(data, plot_data, bootstrapping):
         
         y_model = model_Hobbs(temp, *pfit)
         
-        
         sigma_parameters = np.diagonal(pcov)
 
-        #yerr = propagate_errors(temp, sigma_parameters)
+        yerr = propagate_errors(temp, sigma_parameters, pfit[0])
         
         
-        deltaH_list.append(pfit[1])
-        deltaC_list.append(pfit[2])
-        deltaS_list.append(pfit[3])
+        
+        deltaH_list.append(pfit[0])
+        deltaC_list.append(pfit[1])
+        deltaS_list.append(pfit[2])
         
         sqrt_sigma_parameters = np.sqrt(sigma_parameters)
         
-        deltaH_error.append(sqrt_sigma_parameters[1])
-        deltaC_error.append(sqrt_sigma_parameters[2])
-        deltaS_error.append(sqrt_sigma_parameters[3])
+        deltaH_error.append(sqrt_sigma_parameters[0])
+        deltaC_error.append(sqrt_sigma_parameters[1])
+        deltaS_error.append(sqrt_sigma_parameters[2])
+        
+
 
         
 
         
         ###TEST OF AN ALTERNATIVE MODEL
-        #pfit_2, pcov_2 = curve_fit(model_EEAR, data_set['tempkelvin'], y_true,  p0 = x1, method = 'lm')
-
+        pfit_2, pcov_2 = curve_fit(model_EAAR, data_set['tempkelvin'], y_true, p0 = x1, maxfev = 10000)
+        # bounds=([-np.exp(40), -100,-6,-6], [np.exp(40),100,6,6]
         
-        #y_2 = model_EEAR(temp, *pfit_2)
+        y_2 = model_EAAR(temp, *pfit_2)
+        sigma_2 = np.sqrt(np.diag(pcov_2))
         
+        # yerr_up = model_EAAR(temp, *(pfit_2+sigma_2))
         
-    
-
-#Using Lmfit to fit the models 
+        # yerr_down = model_EAAR(temp, *(pfit_2-sigma_2))
         
-        pars = Parameters()
-        pars.add('deltaH', value=-1000, min=-1000000, max=1000000)
-        pars.add('deltaC', value= -10000, min=-10000, max=1)
-        pars.add('deltaS', value= -100, min=-10000, max=10000)
+        # yerr2 = propagate_errors_2(temp, np.diag(pcov_2), pfit_2[0])
         
-    
-    
-        # model = lmfit.Model(model_Hobbs)
-        
-
-        # result = model.fit(y_true, T=data_set['tempkelvin'], params = pars, weights = 1/sigma)
-        # print(result.fit_report())
-
-        # #now calculate explicit 1-, 2, and 3-sigma uncertainties:
-        # try:
-        #     ci = result.conf_interval(sigmas=[1,2,3])
-        #     lmfit.printfuncs.report_ci(ci)
-        # except:
-        #     print('error')
-        
+        #print(yerr2)
         
         
 
@@ -193,6 +190,14 @@ def fit_model(data, plot_data, bootstrapping):
         Topt = (pfit[0] - (pfit[1]*T0))/(-pfit[1]-R)
         Topt_estim = T0-(pfit[0]/pfit[1])
         Tinf = (pfit[0] - (pfit[1]*T0))/(-pfit[1]+np.sqrt(-pfit[1]*R))
+        
+        #Error propagation
+        Topt_error = (pfit[0]**2 * ((1/pfit[1])**2)) + (pfit[1]**2 * (((R*T0)-pfit[0])/(pfit[1]+R)**2)**2)
+        Topt_error = np.sqrt(Topt_error)
+        
+        Tinf_error = (pfit[0]**2 * (1/(-pfit[1] + np.sqrt(-pfit[1]*R)))**2) + ((pfit[1]**2) * (((2*pfit[0]*np.sqrt(-pfit[1]*R))+ (R*T0*pfit[1])+(R*pfit[0]))/((2*np.sqrt(-R*pfit[1])) * (np.sqrt(-R*pfit[1])-pfit[1])**2))**2)
+        Tinf_error = np.sqrt(Tinf_error)
+        
         
         
         #Converting kelvin to celsius
@@ -204,11 +209,12 @@ def fit_model(data, plot_data, bootstrapping):
         
         #Adding parameters to dictionary
         
-        Tinf_dictionary[i] = Tinf
-        Topt_dictionary[i] = Topt
-        Topt_estim_dictionary[i] = Topt_estim
+        Topt_error_list.append(Topt_error)
+        Topt_list.append(Topt)
+        Tinf_list.append(Tinf)
+        Tinf_error_list.append(Tinf_error)
         
-        
+        print(i + ': ' + str(Tinf) + ' , '+ str(Tinf_error))
         
     
         #Plot model and data if needed
@@ -219,19 +225,17 @@ def fit_model(data, plot_data, bootstrapping):
             
             if index in [0,1,2,3]:
     
-                bound_upper = model_Hobbs(t_true, *(pfit + sigma_parameters))
-                bound_lower = model_Hobbs(t_true, *(pfit - sigma_parameters))
             
                 temp = temp-273.15
         
                 plt.subplot(2, 2, index+1)
                 plt.plot(temp, y_model_plot, label = 'model', linewidth = 2.5, color = 'limegreen')
                 plt.plot(data_set['interactor1temp'], y_true*100, 'o', label='data', markersize = 5.5, color = 'r')
-                #plt.fill_between(t_true-273.15, bound_lower, bound_upper,
-                #        color = 'black', alpha = 0.15, edgecolor = 'black')
-                #plt.fill_between(temp, y_model_plot+yerr, y_model_plot-yerr, color = 'black', alpha = 0.1, edgecolor = 'black')
-        
-            
+                #plt.errorbar(data_set['interactor1temp'], y_true*100,sigma)
+                #plt.fill_between(temp, yerr_up*100, yerr_down*100,
+                #       color = 'black', alpha = 0.15, edgecolor = 'black')
+                #plt.fill_between(temp, (y_2+yerr2)*100, (y_2-yerr2)*100, color = 'black', alpha = 0.1, edgecolor = 'black')
+
                 if bootstrapping == True:
 
                     nboot = 100
@@ -242,7 +246,7 @@ def fit_model(data, plot_data, bootstrapping):
                         p0, cov = curve_fit(model_Hobbs, xb, yb)
                         bspreds[b] = model_Hobbs(t_true,*p0)
                 
-                    plt.plot(t_true, bspreds.T, color = 'C0', alpha = 0.05)
+                    plt.plot(t_true-273.15, bspreds.T*100, color = 'C0', alpha = 0.05)
                 
                 
                 #plt.axvline(x = Topt, color = 'b', linestyle = 'dashed')
@@ -266,10 +270,8 @@ def fit_model(data, plot_data, bootstrapping):
         
 fit_model(data, plot_data = True, bootstrapping = False)
 
-Topt_data = pd.DataFrame.from_dict(Topt_dictionary, orient = 'index')
-Tinf_data = pd.DataFrame.from_dict(Tinf_dictionary, orient = 'index')
-optgrowth_data = pd.DataFrame.from_dict(organism_enzyme_dict, orient = 'index')
 
+#optgrowth_data = pd.DataFrame.from_dict(organism_enzyme_dict, orient = 'index')
 
 
 
@@ -278,8 +280,10 @@ optgrowth_data = pd.DataFrame.from_dict(organism_enzyme_dict, orient = 'index')
 
 def plot_comparison():
     figure(figsize=(8, 7), dpi=3000)
-    plt.scatter(Topt_data[0:18], Topt_data.index[0:18], label = 'Topt')
-    plt.scatter(Tinf_data[0:18], Tinf_data.index[0:18], label = 'Tinf')
+    plt.scatter(Topt_list[0:18], id_list[0:18], label = 'Topt')
+    plt.scatter(Tinf_list[0:18], id_list[0:18], label = 'Tinf')
+    plt.errorbar(Topt_list[0:18], id_list[0:18], xerr=Topt_error_list[0:18], fmt="o")
+    plt.errorbar(Tinf_list[0:18], id_list[0:18], xerr=Tinf_error_list[0:18], fmt="o")
     plt.axvline(x = 30, linestyle = 'dashed')
     plt.legend()
     plt.xlabel('Temperature (°C)', fontsize = 13, fontweight = 'bold')
@@ -291,15 +295,14 @@ def plot_comparison():
 
 
 #Create dataframe for optimal parameter values and their condifence intervals
-# parameters = pd.DataFrame(deltaH_list, index=id_list, columns = ['deltaH'])
-# parameters['deltaC'] = deltaC_list
-# parameters['deltaS'] = deltaS_list
-# parameters['deltaHposerror'] = deltaH_error
-# parameters['deltaCposerror'] = deltaC_error
-# parameters['deltaSposerror'] = deltaS_error
-    
-    
-    
+parameters = pd.DataFrame(deltaH_list, index=id_list, columns = ['deltaH'])
+parameters['deltaC'] = deltaC_list
+parameters['deltaS'] = deltaS_list
+parameters['deltaHposerror'] = deltaH_error
+parameters['deltaCposerror'] = deltaC_error
+parameters['deltaSposerror'] = deltaS_error
+
+
     
 
 
