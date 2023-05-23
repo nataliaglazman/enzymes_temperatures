3a1188d8eaf758cddf0074eb6451f4e77f49c207
Natalia Glazman <glazmann@yahoo.co.uk>
2023-05-23 14:54:11 +0100

    added data normalization, changed curve_fit to lmfit

--- model-fitting-and-plotting.py
+++ model-fitting-and-plotting.py
@@ -36,6 +36,11 @@ data['tempkelvin'] = data['interactor1temp'] + 273.15
 id_list = data.originalid.unique()
 organism_list = data.interactor1.unique()
 
+BS = data['originalid'].loc[data['interactor1'] == 'Bacillus subtilis'].unique()
+EC = data['originalid'].loc[data['interactor1'] == 'Escherichia coli'].unique()
+MT = data['originalid'].loc[data['interactor1'] == 'Mycobacterium tuberculosis'].unique()
+GS = data['originalid'].loc[data['interactor1'] == 'Geobacillus stearothermophilus'].unique()
+
 
 
 #Define set parameters
@@ -61,8 +66,12 @@ deltaH_error = []
 deltaC_error = []
 deltaS_error = []
 enzymes_with_nonnormal_errors = []
+chisquare_mean_EAAR = 0
+chisquare_mean_Hobbs = 0
+sd_Hobbs = 0
+sd_EAAR = 0
 
-        
+
 
         
         
@@ -92,139 +101,163 @@ def test_error_normality(i, y_true, y_predicted):
     alpha = 0.05
     if p <= alpha:
         enzymes_with_nonnormal_errors.append(i)
-        
 
 
-def fit_model(data, plot_data, bootstrapping):
+
+#Model fitting and plots
+
+def fit_model(data, plot_data, bootstrapping, model, overlay_plot, overlay_plot_organism):
     x0 = -100,1.3,-100
-    x1 = 14000, 0.58, 0.8, -0.085
+    x1 = np.exp(10), 0.8, 2.50, 0.085
     figure(figsize=(12, 10), dpi=3000)
-    
+    chisquare_Hobbs = []
+    chisquare_EAAR = []
     
     for index, i in enumerate(id_list):
         
         def model_Hobbs(T, deltaH, deltaC, deltaS):
             A = ((kB*T)/(h)) * np.exp(-((deltaH+(deltaC*(T-T0)))/(R*T)) + ((deltaS+(deltaC*np.log(T/T0)))/R))
             return A
-                
+            
         def model_EAAR(T, A0, eb, ef, ehc):  
-            A = A0*np.exp(-((eb-(ef*(1-(T/T0)))+(ehc*(T-T0-(T*np.log(T/T0)))))/(k*T)))
+            #A = A0*np.exp(-((eb-(ef*(1-(T/T0)))+(ehc*(T-T0-(T*np.log(T/T0)))))/(k*T)))
+            A = np.log(A0) - ((eb - ef*(1-(T/T0)) - ehc*(T-T0-(T*np.log(T/T0))))/(T*k))
             return A
         
                 
         data_set = data.loc[data['originalid'] == i]
-        T0 = data_set['tempkelvin'].loc[data_set['originaltraitvalue'].idxmax()] + 4
         tmin = data_set['tempkelvin'].min() 
         tmax = data_set['tempkelvin'].max()
-                
+        dt = 0.5
+        temp=np.arange(tmin, tmax, dt)
+        t_true = data_set['tempkelvin'].array
+        
+    
                 
         #Normalize non-relative data 
         if data_set['originaltraitname'].array[0] != 'relative activity':
             y_true = (data_set['originaltraitvalue'] - data_set['originaltraitvalue'].min()) / (data_set['originaltraitvalue'].max() - data_set['originaltraitvalue'].min())
-            y_true = y_true *100
+            y_true = y_true*100
         else:
             y_true = data_set['originaltraitvalue']
                     
-            
-                
-        #Create a fake temperature parameter for model plots
-        dt = 0.5
-        temp=np.arange(tmin, tmax, dt)
-        t_true = data_set['tempkelvin'].array
-            
+        
+        y_true = y_true.apply(lambda x: x+1 if x == 0 else x)
         
         
         #Model fitting using curve fit
-
-        pfit, pcov = curve_fit(model_Hobbs, t_true,  y_true, p0 = x0, method = 'lm')
         
-        y_predicted = model_Hobbs(t_true, *pfit)
-        y_model = model_Hobbs(temp, *pfit)
+        if model == 'Hobbs':
+            
+            T0 = data_set['tempkelvin'].loc[data_set['originaltraitvalue'].idxmax()] + 4
+            
+            pfit, pcov = curve_fit(model_Hobbs, t_true, y_true, p0 = x0, method = 'lm')
         
-        
-        #Error estimation and propagation
-        test_error_normality(i, y_true, y_predicted)
-        
-        sigma_parameters = np.diagonal(pcov)
-        yerr = propagate_errors(temp, T0, sigma_parameters)
-        # yerr_up = y_model + yerr
-        # yerr_down = y_model - yerr
-        y_true = data_set['originaltraitvalue'].array
-        chi_sqrt = 0
-        for i in range(len(y_true)):
-            chi_sqrt = chi_sqrt +  ((y_true[i] - y_predicted[i])**2)/y_predicted[i]
-        #chi_Hobbs = np.sqrt((y_true-y_predicted)**2/y_predicted)
-        #chi_Hobbs = chisquare(y_true, y_predicted)
-        
-        
-        print(chi_sqrt)
-        
-        ###TEST OF EAAR MODEL
-        pfit_2, pcov_2 = curve_fit(model_EAAR, t_true, y_true, p0 = x1)
-        
-        y_2 = model_EAAR(temp, *pfit_2)
-        y_predicted_2 = model_EAAR(t_true, *pfit_2)
-        sigma_2 = np.sqrt(np.diag(pcov_2))
-        #chi_EAAR = np.sqrt((y_true-y_predicted_2)/y_predicted_2)
-        
-        # yerr_up = model_EAAR(temp, *(pfit_2+sigma_2))
-        
-        # yerr_down = model_EAAR(temp, *(pfit_2-sigma_2))
-        
-        yerr2 = propagate_errors_2(temp, T0, np.diag(pcov_2), pfit_2[0])
-        yerr_up = y_2 + yerr2
-        yerr_down = y_2 - yerr2
-        
-        
-
+            y_predicted = model_Hobbs(t_true, *pfit)
+            y_model = model_Hobbs(temp, *pfit)
+            sigma_parameters = np.diagonal(pcov)
+            yerr = propagate_errors(temp, T0, sigma_parameters)
+            yerr_up = y_model + yerr
+            yerr_down = y_model - yerr
+            
+            
         #Estimation of Topt and Tinf parameters
         
-        Topt = (pfit[0] - (pfit[1]*T0))/(-pfit[1]-R)
-        Topt_estim = T0-(pfit[0]/pfit[1])
-        Tinf = (pfit[0] - (pfit[1]*T0))/(-pfit[1]+np.sqrt(-pfit[1]*R))
+            Topt = (pfit[0] - (pfit[1]*T0))/(-pfit[1]-R)
+            Topt_estim = T0-(pfit[0]/pfit[1])
+            Tinf = (pfit[0] - (pfit[1]*T0))/(-pfit[1]+np.sqrt(-pfit[1]*R))
+            
+            Topt = Topt-273.15
+            Tinf = Tinf-273.15
+            Topt_estim = Topt_estim - 273.15
+            
+            #Error propagation 
+            alpha = 0.05
+            N = len(y_true)
+            P = len(pfit)
+            dof = max(0,N-P)
+            tval = t.ppf((1-alpha/2), dof)
+            
+            Topt_error = (pfit[0]**2 * ((1/pfit[1])**2)) + (pfit[1]**2 * (((R*T0)-pfit[0])/(pfit[1]+R)**2)**2)
+            Topt_error = np.sqrt(np.sqrt(Topt_error))
+            Topt_confint = Topt_error * tval
+            
+            Tinf_error = (pfit[0]**2 * (1/(-pfit[1] + np.sqrt(-pfit[1]*R)))**2) + ((pfit[1]**2) * (((2*pfit[0]*np.sqrt(-pfit[1]*R))+ (R*T0*pfit[1])+(R*pfit[0]))/((2*np.sqrt(-R*pfit[1])) * (np.sqrt(-R*pfit[1])-pfit[1])**2))**2)
+            Tinf_error = np.sqrt(np.sqrt(Tinf_error))
+            Tinf_confint = Tinf_error * tval
+            
+            Topt_list.append(Topt)
+            Tinf_list.append(Tinf)
+            Tinf_confint_list.append(Tinf_confint)
+            Topt_confint_list.append(Topt_confint)
+            
+            
+            # #LMFIT trial
+            
+            lmfit_Hobbs = Model(model_Hobbs)
+            pars = lmfit.Parameters()
+            pars.add_many(('deltaH', -100), ('deltaC', 1.3), ('deltaS', -100))
+            result = lmfit_Hobbs.fit(y_true/100, T=t_true,params=pars)
+             #print(result.fit_report())
+            chisquare_Hobbs.append(result.chisqr)
+            chisquare_mean_Hobbs = sum(chisquare_Hobbs)/len(chisquare_Hobbs)
+            sd_Hobbs = np.std(chisquare_Hobbs)
+            #y_predicted = result.best_fit
+            
+            
+            
+            
+        elif model == 'EAAR':
+            
+            #Find tref for EAAR model
+            ref_data = data_set['tempkelvin'].loc[data_set['originaltraitvalue'].idxmax():]
+            ref_data1 = data_set['originaltraitvalue'].loc[data_set['originaltraitvalue'].idxmax():]
+            fit = np.polyfit(ref_data, ref_data1, 1)
+            roots = np.roots(fit)
+            T0 = roots - 273.15
+            
+            #Fit model
+            pfit, pcov = curve_fit(model_EAAR, t_true, y_true, p0 = x1, maxfev=100000)
+            y_model = model_EAAR(temp, *pfit)
+            y_predicted = model_EAAR(t_true, *pfit)
+            sigma_parameters = np.sqrt(np.diag(pcov))
+            yerr = propagate_errors_2(temp, T0, np.diag(pcov), pfit[0])
+            yerr_up = y_model + yerr
+            yerr_down = y_model - yerr
+            
+            #LMFIT trial
+            y_true = y_true/100
+            model_2 = Model(model_EAAR)
+            param = lmfit.Parameters()
+            param.add_many(('A0', 14, True, 0, np.exp(20)), ('eb', 0.1, True, -2, 5), ('ef', 2.59, True, -2, 10), ('ehc', 0.085, True, -1, 2))
+            result = model_2.fit(y_true, T=t_true, params=param)
+            chisquare_EAAR.append(result.chisqr)
+            chisquare_mean_EAAR = sum(chisquare_EAAR)/len(chisquare_EAAR)
+            sd_EAAR = np.std(chisquare_EAAR)
+            print(chisquare_mean_EAAR)
+            
+            
+            
+            
+            
+        #Error estimation and propagation
+        #test_error_normality(i, y_true, y_predicted)
         
         
-        Topt = Topt-273.15
-        Tinf = Tinf-273.15
-        Topt_estim = Topt_estim - 273.15
+        #Calculate chi-squared 
+        # y_true = data_set['originaltraitvalue'].array
+        # chi_sqrt = 0
+        # for p in range(len(y_true)):
+        #     chi_sqrt = chi_sqrt +  (((y_true[p] - y_predicted[p]))/yerr[p])**2
         
-        #Error propagation 
-        alpha = 0.05
-        N = len(y_true)
-        P = len(pfit)
-        dof = max(0,N-P)
-        tval = t.ppf((1-alpha/2), dof)
-        
-        
-        Topt_error = (pfit[0]**2 * ((1/pfit[1])**2)) + (pfit[1]**2 * (((R*T0)-pfit[0])/(pfit[1]+R)**2)**2)
-        Topt_error = np.sqrt(np.sqrt(Topt_error))
-        Topt_confint = Topt_error * tval
-        
-        Tinf_error = (pfit[0]**2 * (1/(-pfit[1] + np.sqrt(-pfit[1]*R)))**2) + ((pfit[1]**2) * (((2*pfit[0]*np.sqrt(-pfit[1]*R))+ (R*T0*pfit[1])+(R*pfit[0]))/((2*np.sqrt(-R*pfit[1])) * (np.sqrt(-R*pfit[1])-pfit[1])**2))**2)
-        Tinf_error = np.sqrt(np.sqrt(Tinf_error))
-        Tinf_confint = Tinf_error * tval
-    
-        
-        #Adding parameters to dictionary
-        
-        
-        Topt_list.append(Topt)
-        Tinf_list.append(Tinf)
-        Tinf_confint_list.append(Tinf_confint)
-        Topt_confint_list.append(Topt_confint)
-        
-        #print(i + ': ' + str(pcov) + ' , ')
-    
-
 
         #Plot model and data if needed
-        
+        y = y_model
+        temp = temp-273.15
         
         if plot_data == True:
             
-            y = y_predicted
             
-            temp = temp-273.15
             index_list = list(range(4, len(id_list), 5))
             index_list_2 = list(range(1, len(id_list), 5))
             index_list_3 = list(range(2, len(id_list), 5))
@@ -241,8 +274,8 @@ def fit_model(data, plot_data, bootstrapping):
                 plt.title(i[0:3], fontsize = 15, fontweight = 'bold')
                 plt.xlabel('Temperature (°C)', fontsize = 13, fontweight = 'bold')
                 plt.ylabel('Relative activity (%)', fontsize = 13, fontweight = 'bold')
-                plt.axvline(x = Topt, color = 'b', linestyle = 'dashed', label = 'Topt')
-                plt.axvline(x = Tinf, color = 'g', linestyle = 'dashed', label = 'Tinf')
+                #plt.axvline(x = Topt, color = 'b', linestyle = 'dashed', label = 'Topt')
+                #plt.axvline(x = Tinf, color = 'g', linestyle = 'dashed', label = 'Tinf')
                 plt.legend()
             
             if index in index_list:
@@ -263,8 +296,8 @@ def fit_model(data, plot_data, bootstrapping):
                 plt.title(i[0:3], fontsize = 15, fontweight = 'bold')
                 plt.xlabel('Temperature (°C)', fontsize = 13, fontweight = 'bold')
                 plt.ylabel('Relative activity (%)', fontsize = 13, fontweight = 'bold')
-                plt.axvline(x = Topt, color = 'b', linestyle = 'dashed', label = 'Topt')
-                plt.axvline(x = Tinf, color = 'g', linestyle = 'dashed', label = 'Tinf')
+                #plt.axvline(x = Topt, color = 'b', linestyle = 'dashed', label = 'Topt')
+                #plt.axvline(x = Tinf, color = 'g', linestyle = 'dashed', label = 'Tinf')
                 plt.legend()
                 
             elif index in index_list_2:
@@ -277,8 +310,8 @@ def fit_model(data, plot_data, bootstrapping):
                 plt.title(i[0:3], fontsize = 15, fontweight = 'bold')
                 plt.xlabel('Temperature (°C)', fontsize = 13, fontweight = 'bold')
                 plt.ylabel('Relative activity (%)', fontsize = 13, fontweight = 'bold')
-                plt.axvline(x = Topt, color = 'b', linestyle = 'dashed', label = 'Topt')
-                plt.axvline(x = Tinf, color = 'g', linestyle = 'dashed', label = 'Tinf')
+                #plt.axvline(x = Topt, color = 'b', linestyle = 'dashed', label = 'Topt')
+                #plt.axvline(x = Tinf, color = 'g', linestyle = 'dashed', label = 'Tinf')
                 plt.legend()
                 
             elif index in index_list_3:
@@ -290,8 +323,8 @@ def fit_model(data, plot_data, bootstrapping):
                 plt.title(i[0:3], fontsize = 15, fontweight = 'bold')
                 plt.xlabel('Temperature (°C)', fontsize = 13, fontweight = 'bold')
                 plt.ylabel('Relative activity (%)', fontsize = 13, fontweight = 'bold')
-                plt.axvline(x = Topt, color = 'b', linestyle = 'dashed', label = 'Topt')
-                plt.axvline(x = Tinf, color = 'g', linestyle = 'dashed', label = 'Tinf')
+                #plt.axvline(x = Topt, color = 'b', linestyle = 'dashed', label = 'Topt')
+                #plt.axvline(x = Tinf, color = 'g', linestyle = 'dashed', label = 'Tinf')
                 plt.legend()
                 
             elif index in index_list_4:
@@ -303,47 +336,80 @@ def fit_model(data, plot_data, bootstrapping):
                 plt.title(i[0:3], fontsize = 15, fontweight = 'bold')
                 plt.xlabel('Temperature (°C)', fontsize = 13, fontweight = 'bold')
                 plt.ylabel('Relative activity (%)', fontsize = 13, fontweight = 'bold')
-                plt.axvline(x = Topt, color = 'b', linestyle = 'dashed', label = 'Topt')
-                plt.axvline(x = Tinf, color = 'g', linestyle = 'dashed', label = 'Tinf')
+                #plt.axvline(x = Topt, color = 'b', linestyle = 'dashed', label = 'Topt')
+                #plt.axvline(x = Tinf, color = 'g', linestyle = 'dashed', label = 'Tinf')
                 plt.legend()
             
                 
-                # if bootstrapping == True:
+                if bootstrapping == True:
 
-                #     nboot = 100
-                #     bspreds = np.zeros((nboot, y_true.size))
+                    nboot = 100
+                    bspreds = np.zeros((nboot, y_true.size))
         
-                #     for b in range(nboot):
-                #         xb,yb = bootstrap(t_true,y_true)
-                #         p0, cov = curve_fit(model_Hobbs, xb, yb)
-                #         bspreds[b] = model_Hobbs(t_true,*p0)
+                    for b in range(nboot):
+                        xb,yb = bootstrap(t_true,y_true)
+                        p0, cov = curve_fit(model_Hobbs, xb, yb)
+                        bspreds[b] = model_Hobbs(t_true,*p0)
                 
-                #     plt.plot(t_true-273.15, bspreds.T*100, color = 'C0', alpha = 0.05)
+                    plt.plot(t_true-273.15, bspreds.T*100, color = 'C0', alpha = 0.05)
+                    
+                    
+        if overlay_plot == True:
+            
+            if index != len(overlay_plot_organism):
                 
-                
-
-
-
+                plt.plot(temp, y, label = 'model', linewidth = 2.5, color = 'limegreen')
+                #plt.plot(data_set['interactor1temp'], y_true, 'o', label='data', markersize = 5.5, color = 'r')
+                #plt.fill_between(temp, yerr_up, yerr_down,
+                #   color = 'black', alpha = 0.15, edgecolor = 'black')
+                plt.xlabel('Temperature (°C)', fontsize = 13, fontweight = 'bold')
+                plt.ylabel('Relative activity (%)', fontsize = 13, fontweight = 'bold')
         
-fit_model(data, plot_data = False, bootstrapping = False)
+            else:
+                plt.plot(temp, y, label = 'model', linewidth = 2.5, color = 'limegreen')
+                #plt.plot(data_set['interactor1temp'], y_true, 'o', label='data', markersize = 5.5, color = 'r')
+                #plt.fill_between(temp, yerr_up, yerr_down,
+                #   color = 'black', alpha = 0.15, edgecolor = 'black')
+                plt.xlabel('Temperature (°C)', fontsize = 13, fontweight = 'bold')
+                plt.ylabel('Relative activity (%)', fontsize = 13, fontweight = 'bold')
+                plt.legend()
+                plt.rcParams['figure.dpi'] = 2000
+                plt.rcParams['savefig.dpi'] = 2000
+                plt.title('Hobbs et al. model fit', fontsize = 25)
+                plt.tight_layout()
+                plt.show()
+                
+                
+                
+        
+
+
+fit_model(data, plot_data = False, bootstrapping = False, model = 'Hobbs', overlay_plot = False, overlay_plot_organism = MT)
+
+
+id_list_short = []
+for i in id_list:
+    p = i[0:3]
+    id_list_short.append(p)
 
 
 #Plotting relationship between Topt and optimal growth temp for each organism
 
 def plot_comparison():
-    figure(figsize=(8, 7), dpi=3000)
-    plt.scatter(Topt_list[0:18], id_list[0:18], label = 'Topt')
-    plt.scatter(Tinf_list[0:18], id_list[0:18], label = 'Tinf')
-    plt.errorbar(Topt_list[0:18], id_list[0:18], xerr=Topt_confint_list[0:18], fmt="o",capsize = 3)
-    plt.errorbar(Tinf_list[0:18], id_list[0:18], xerr=Tinf_confint_list[0:18], fmt="o", capsize = 3)
+    figure(figsize=(8, 7), dpi=4000)
+    plt.scatter(Topt_list[0:18], id_list_short[0:18], label = 'Topt', c = 'tomato')
+    plt.scatter(Tinf_list[0:18], id_list_short[0:18], label = 'Tinf', c = 'cornflowerblue')
+    plt.errorbar(Topt_list[0:18], id_list_short[0:18], xerr=Topt_confint_list[0:18], fmt="o",capsize = 3, c='tomato')
+    plt.errorbar(Tinf_list[0:18], id_list_short[0:18], xerr=Tinf_confint_list[0:18], fmt="o", capsize = 3, c = 'cornflowerblue')
     plt.axvline(x = 30, linestyle = 'dashed', label = 'Optimal growth temperature')
-    plt.legend()
+    plt.legend(loc='upper center', bbox_to_anchor=(0.5, 1.05),
+          ncol=3, fancybox=True, shadow=True)
     plt.xlabel('Temperature (°C)', fontsize = 13, fontweight = 'bold')
     plt.ylabel('Enzyme ID', fontsize = 13, fontweight = 'bold')
-    plt.title('Topt and Tinf comparison to optimal\n' + r'growth temperature of B. subtilis', fontsize = 15, fontweight = 'bold')
+    #plt.title('$T_{opt}$ and $T_{inf}$ comparison to optimal\n' + r'growth temperature of B. subtilis', fontsize = 15)
     plt.show()
     
-#plot_comparison()
+plot_comparison()
 
     
 
